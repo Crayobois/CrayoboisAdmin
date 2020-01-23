b1154fea3bdb119f6a11106a3e7e90f144b4b0de
Zachary Legros <legroszach3921@gmail.com>
2020-01-23 17:52:11 -0500

    displayed order info

--- client/src/components/dashboard/FocusedOrder.js
+++ client/src/components/dashboard/FocusedOrder.js
@@ -3,15 +3,62 @@ import "./FocusedOrder.css";
 import AuthContext from "../context/AuthContext";
 
 const FocusedOrder = props => {
+  let order = props.order;
 
+  const parseDate = prevDate => {
+    let allowedChar = "0123456789-";
+    let newDate = "";
+
+    for (let e = 0; e < prevDate.length; e++) {
+      if (allowedChar.includes(prevDate[e])) {
+        newDate += prevDate[e];
+      } else {
+        break;
+      }
+    }
+
+    return newDate;
+  };
+
+  const priceFormatter = new Intl.NumberFormat("fr-CA", {
+    style: "currency",
+    currency: "CAD",
+    minimumFractionDigits: 2
+  });
 
   return (
     <div className="focused-order-container">
-      <span className="focused-order-back" onClick={() => {
-        props.removeFocus();
-      }}><i className="fas fa-chevron-left focused-order-back-icon"></i>Précédent</span>
+      <div className="focused-order-top">
+        <span
+          className="focused-order-back"
+          onClick={() => {
+            props.removeFocus();
+          }}
+        >
+          <i className="fas fa-chevron-left focused-order-back-icon"></i>
+          Précédent
+        </span>
+        <span className="filter-btn">
+          Livrer<i className="fas fa-shipping-fast ship"></i>
+        </span>
+      </div>
       <div className="focused-order">
-
+        <div className="order-info">
+          <div className="order-info-client">
+            <span>{`${order.payer.name.given_name} ${order.payer.name.surname}`}</span>
+            <span>{order.payer.email_address}</span>
+            <span>{parseDate(order.create_time)}</span>
+            <span>
+              {order.purchase_units[0].shipping.address.address_line_1}
+            </span>
+            <span>
+              {`${order.purchase_units[0].shipping.address.admin_area_2}, ${order.purchase_units[0].shipping.address.admin_area_1}, ${order.purchase_units[0].shipping.address.country_code}`}
+            </span>
+            <span>{order.purchase_units[0].shipping.address.postal_code}</span>
+          </div>
+          <div className="order-info-summary"></div>
+        </div>
+        <div className="order-bill"></div>
       </div>
     </div>
   );
