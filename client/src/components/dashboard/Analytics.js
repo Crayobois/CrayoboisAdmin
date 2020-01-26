55d026136cf2fe48e8d70d8f89877e988f272dd2
Zachary Legros <legroszach3921@gmail.com>
2020-01-26 16:24:21 -0500

    added rough revenu insights

--- client/src/components/dashboard/Analytics.js
+++ client/src/components/dashboard/Analytics.js
@@ -5,6 +5,13 @@ import "./Analytics.css";
 const Analytics = props => {
   const context = useContext(AuthContext);
 
+  const priceFormatter = new Intl.NumberFormat("fr-CA", {
+    style: "currency",
+    currency: "CAD",
+    minimumFractionDigits: 2
+  });
+
+
   return (
     <section className="analytics-section">
       <div className="customers-insights">
@@ -15,6 +22,24 @@ const Analytics = props => {
         <div className="insights insights-orders">
           <span className="insights-name">Commandes</span>
           <span className="insights-value">256</span>
+        </div>
+      </div>
+      <div className="revenue-insights">
+        <div className="insights insights-clients">
+          <span className="insights-name">Revenu brut</span>
+          <span className="insights-value">{priceFormatter.format(10050.25)}</span>
+        </div>
+        <div className="insights insights-orders revenu-right">
+          <span className="insights-name">TPS</span>
+          <span className="insights-value">{priceFormatter.format(17.20)}</span>
+        </div>
+        <div className="insights insights-orders revenu-right">
+          <span className="insights-name">TVQ</span>
+          <span className="insights-value">{priceFormatter.format(25.35)}</span>
+        </div>
+        <div className="insights insights-orders">
+          <span className="insights-name">Livraison</span>
+          <span className="insights-value">{priceFormatter.format(15.25)}</span>
         </div>
       </div>
     </section>
